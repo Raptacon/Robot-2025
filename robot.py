eed6d429e5f08290fcf4bb8466b8db43050bbcc1
Programming Garrett <147673732+DanteSGarrett@users.noreply.github.com>
2025-02-04 19:22:14 -0700

    Initial Swerve Code 2025 (#64)
    * Swerve: clear out old swerve code
    
    * Swerve: Added working swerve from pathplanner-complete branch
    
    * Swerve: robotswerve file has been added
    
    * Requirements: update to 2025
    
    * Swerve: made robot.py and robotswerve.py compatible
    
    * Requirements: update toml for new 2025 sync:
    
    * Swerve: add docstrings to the drivetrain class
    
    * Swerve: rename module class based on ture motors used
    
    * PathPlanner: create a test routine to confirm PathPlanner works with migrated code
    
    * PathPlanner: create a test routine to confirm PathPlanner works with migrated code
    
    * Swerve: additioanl cleanup
    
    * General: clean up broken imports
    
    * Telmetry: update and reincorporate telemetry for 2025
    
    * Telemetry: mimic 2024 call patterns
    
    * Swerve: use button input for flipping to robot-relative drive
    
    * Swerve: use button input for flipping to robot-relative drive
    
    * Swerve: move around some configuration parameters
    
    * Requirements: include numpy
    
    * Requirements: enforce 2025
    
    * Swerve: setup configurations for 2025 drive
    
    * Swerve: fix bug in default drive where field was not called
    
    * PathPlanner: update GUI config
    
    * Swerve: add docstrings to default drive command
    
    * Swerve: tune drive PID feedforward term to ensure max velocity can be hit
    
    * Swerve: enable absolute encoder in steer positioning
    
    * Swerve: enable cosine scaling
    
    * PathPlanner: lower default max velocity
    
    * Swerve: clean out print statements
    
    * refactor: 💡 moved constants in sensors to raptacon3200
    
    * Swerve: added units to the config and constants files
    
    ---------
    
    Co-authored-by: Jordan Durham <jtd_29@yahoo.com>
    Co-authored-by: Chris Hirsch <chris@kiatek.com>

--- robot.py
+++ robot.py
@@ -13,24 +13,22 @@ class MyRobot(commands2.TimedCommandRobot):
     has an implementation of (self) -> None:
     which runs the scheduler for you
     """
-    #50 ms default period
+    # 50 ms default period
     kDefaultPeriod: typing.ClassVar[float] = 50.0
     autonomousCommand: typing.Optional[commands2.Command] = None
 
     def __init__(self) -> None:
-        #setup our scheduling period. Defaulting to 20 Hz (50 ms)
+        # setup our scheduling period. Defaulting to 20 Hz (50 ms)
         super().__init__(period=MyRobot.kDefaultPeriod / 1000)
-        #super().__init__()
-
 
     def robotInit(self) -> None:
         """
         This function is run when the robot is first started up and should be used for any
         initialization code.
         """
-        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # Instantiate our RobotContainer. This will perform all our button bindings, and put our
         # autonomous chooser on the dashboard.
-        self.container = RobotSwerve()
+        self.container = RobotSwerve(lambda: self.isDisabled)
 
     def robotPeriodic(self) -> None:
         self.container.robotPeriodic()
@@ -46,22 +44,12 @@ class MyRobot(commands2.TimedCommandRobot):
     def autonomousInit(self) -> None:
         """This autonomous runs the autonomous command selected by your RobotContainer class."""
         self.container.autonomousInit()
-        #self.autonomousCommand = self.container.getAutonomousCommand()
-
-        #if self.autonomousCommand:
-        #    self.autonomousCommand.schedule()
 
     def autonomousPeriodic(self) -> None:
         """This function is called periodically during autonomous"""
         self.container.autonomousPeriodic()
 
     def teleopInit(self) -> None:
-        # This makes sure that the autonomous stops running when
-        # teleop starts running. If you want the autonomous to
-        # continue until interrupted by another command, remove
-        # this line or comment it out.
-        #if self.autonomousCommand:
-        #    self.autonomousCommand.cancel()
         self.container.teleopInit()
 
     def teleopPeriodic(self) -> None:
@@ -69,8 +57,6 @@ class MyRobot(commands2.TimedCommandRobot):
         self.container.teleopPeriodic()
 
     def testInit(self) -> None:
-        # Cancels all running commands at the start of test mode
-        #commands2.CommandScheduler.getInstance().cancelAll()
         self.container.testInit()
 
     def testPeriodic(self) -> None:
@@ -80,4 +66,3 @@ class MyRobot(commands2.TimedCommandRobot):
 if __name__ == "__main__":
     print("Please run python -m robotpy <args>")
     exit(1)
-
