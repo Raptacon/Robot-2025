e652ca7f2091bfb508d3a5cbfc83e23a63a5df90
Jordan Durham <jtd_29@yahoo.com>
2025-02-22 14:29:32 -0700

    Sparky: increase tolerance on auto align

--- config.py
+++ config.py
@@ -35,11 +35,11 @@ class OperatorRobotConfig:
     # Translation trapezoidal profile units are mps and mps^2, rotation are rps and rps^2
     pid_to_pose_translation_pid_profile: Tuple[float] = (2.0, 0.0, 0.0, 3, 1.5)
     pid_to_pose_rotation_pid_profile: Tuple[float] = (
-        1.0, 0.0, 0.0, Rotation2d.fromDegrees(360).radians(), Rotation2d.fromDegrees(360).radians()
+        0.5, 0.0, 0.0, Rotation2d.fromDegrees(360).radians(), Rotation2d.fromDegrees(360).radians()
     )
     # Tolerance of x, y, and omega position errors within which robot is at target pose
     # x error is in meters, y error is in meters, omega error is in radians
-    pid_to_pose_setpoint_tolerances: Tuple[float] = (0.25, 0.25, Rotation2d.fromDegrees(10).radians())
+    pid_to_pose_setpoint_tolerances: Tuple[float] = (0.5, 0.5, Rotation2d.fromDegrees(50).radians())
 
     # Robot motion constraints when running PathPlanner during teleop.
     # Values to give are: max translation velocity (mps), max translation acceleration (mps^2),
