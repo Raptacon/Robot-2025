ebc91a29a69e68a5d3443bd0bbf11b324c16392c
Chris Hirsch <chris@kiatek.com>
2025-03-10 18:51:59 -0600

    feat: 🎸 initial SM for pivot - UNTESTED ON REAL BOT

--- test_armRobot.py
+++ test_armRobot.py
@@ -13,13 +13,21 @@ class MyRobot(wpilib.TimedRobot):
         """Robot initialization function"""
         self.arm = diverCarlChistera.DiverCarlChistera()
 
+        self.timer = wpilib.Timer()
+        self.timer.start()
+
+    def disabledInit(self):
+        self.arm.on_disable()
 
     def testInit(self):
         wpilib.SmartDashboard.putNumber("Arm Rad", 0.0)
         wpilib.SmartDashboard.putNumber("Arm Deg", 0.0)
+        self.arm.on_enable()
 
     def testPeriodic(self):
         self.arm.periodic()
         wpilib.SmartDashboard.putNumber("Arm Deg", self.arm.getAngleDeg())
 
-        #print("Encoder Pos", self.elevator.getPosDeg())
+        self.arm.on_iteration(self.timer.get())
+
+        # print("Encoder Pos", self.elevator.getPosDeg())
