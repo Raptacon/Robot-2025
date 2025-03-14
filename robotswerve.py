841bd853621bf17f2f550deb0d7a1273a84368b0
Jordan Durham <jtd_29@yahoo.com>
2025-03-14 13:05:26 -0600

    Swerve: flip boolean on turbo mode

--- robotswerve.py
+++ robotswerve.py
@@ -176,7 +176,7 @@ class RobotSwerve:
                 lambda: wpimath.applyDeadband(-1 * self.driver_controller.getLeftX(), 0.06),
                 lambda: wpimath.applyDeadband(-1 * self.driver_controller.getRightX(), 0.1),
                 lambda: not self.driver_controller.getRightBumperButton(),
-                lambda: not self.driver_controller.getLeftBumperButton()
+                lambda: self.driver_controller.getLeftBumperButton()
             )
         )
 
