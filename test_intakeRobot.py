bb26ea799a779f20f114ce1438944d5f5305e922
Jordan Durham <jtd_29@yahoo.com>
2025-03-20 20:56:06 -0600

    Intake: update test class for commands (commands won't execute in test mode

--- test_intakeRobot.py
+++ test_intakeRobot.py
@@ -4,7 +4,8 @@ from subsystem.captainIntake import CaptainIntake
 import commands.operate_intake as IntakeCommands
 import commands2
 
-class MyRobot(wpilib.TimedRobot):
+
+class MyRobot(commands2.TimedCommandRobot):
     """
     Example robot to test the intake
     """
@@ -16,29 +17,44 @@ class MyRobot(wpilib.TimedRobot):
         self.driver_controller = wpilib.XboxController(0)
         self.mech_controller = wpilib.XboxController(1)
 
-    def disabledInit(self):
-        commands2.CommandScheduler.getInstance().cancelAll()
-
-    def testInit(self):
-        commands2.CommandScheduler.getInstance().cancelAll()
+    def teleopInit(self):
         commands2.cmd.sequence(
-            IntakeCommands.IntakeToFront(self.intake, holdDurationSeconds=0.1, reverse=False),
+            commands2.DeferredCommand(
+                IntakeCommands.IntakeToFront(self.intake, holdDurationSeconds=0.1, reverse=False),
+                self.intake
+            ),
             commands2.cmd.waitSeconds(1),
-            IntakeCommands.IntakeToFrontOnly(self.intake),
+            commands2.DeferredCommand(
+                IntakeCommands.IntakeToFrontOnly(self.intake), self.intake
+            ),
             commands2.cmd.waitSeconds(0.5),
-            IntakeCommands.IntakeToBack(self.intake, holdDurationSeconds=0.1, reverse=True),
+            commands2.DeferredCommand(
+                IntakeCommands.IntakeToBack(self.intake, holdDurationSeconds=0.1, reverse=True), self.intake
+            ),
             commands2.cmd.waitSeconds(1),
-            IntakeCommands.IntakeToFrontOnly(self.intake),
+            commands2.DeferredCommand(
+                IntakeCommands.IntakeToFrontOnly(self.intake), self.intake
+            ),
             commands2.cmd.waitSeconds(0.5),
-            IntakeCommands.generateIntakeMaintainHold(self.intake),
+            commands2.DeferredCommand(
+                IntakeCommands.generateIntakeMaintainHold(self.intake), self.intake
+            ),
             commands2.cmd.waitSeconds(1),
-            IntakeCommands.generateIntakeFrontForwardHold(self.intake),
+            commands2.DeferredCommand(
+                IntakeCommands.generateIntakeFrontForwardHold(self.intake), self.intake
+            ),
             commands2.cmd.waitSeconds(1)
         ).schedule()
         self.intake.setDefaultCommand(IntakeCommands.IntakeManually(
             lambda: wpimath.applyDeadband(self.mech_controller.getRightTriggerAxis(), 0.1),
             self.intake
         ))
+
+    def disabledInit(self):
+        commands2.CommandScheduler.getInstance().cancelAll()
+
+    def testInit(self):
+        commands2.CommandScheduler.getInstance().cancelAll()
 
     def testPeriodic(self):
         pass
